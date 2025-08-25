c4e1ab2be0990728e9432e2de22a1699e4e0d2ed
ralalem-debug <r.alalem@ow-jo.com>
2025-08-25 12:17:42 +0300

    edit1

--- hr_system_/lib/main.dart
+++ hr_system_/lib/main.dart
@@ -67,6 +67,8 @@ class OnsetWayApp extends StatelessWidget {
         GetPage(name: '/profile', page: () => ProfileScreen()),
         GetPage(name: '/aboutus', page: () => AboutUsScreen()),
         GetPage(name: '/privacypolicy', page: () => PrivacyPolicyScreen()),
+
+        GetPage(name: '/privacypolicy', page: () => PrivacyPolicyScreen()),
       ],
     );
   }
