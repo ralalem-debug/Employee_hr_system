10d68838a12b680a9cf08d0f9520ab6bde61ee8f
ralalem-debug <r.alalem@ow-jo.com>
2025-08-26 09:10:23 +0300

    edit1

--- hr_system_/macos/Flutter/GeneratedPluginRegistrant.swift
+++ hr_system_/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import device_info_plus
 import file_picker
 import file_selector_macos
 import flutter_image_compress_macos
+import flutter_secure_storage_macos
 import gal
 import just_audio
 import media_kit_video
@@ -36,6 +37,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
--- hr_system_/windows/flutter/generated_plugins.cmake
+++ hr_system_/windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
   desktop_drop
   file_selector_windows
+  flutter_secure_storage_windows
   gal
   media_kit_video
   record_windows
